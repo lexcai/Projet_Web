68757686b9c8123c07e48daa4a040b2a89913ea4
axel.boudeau@ynov.com <axel.boudeau@ynov.com>
2020-06-03 16:51:12 +0200

    Ajout moderation membres page a completer en partie par Marius

---
+++ moderation_membres.php
@@ -0,0 +1,50 @@
+<?php
+include_once('bdd.php');
+session_start();
+  $query1 = $pdo->prepare('SELECT * FROM membres');
+  $query1->execute();
+  $liste = $query1->fetchAll(); 
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+ <h1>Liste des membres</h1>
+ <table>
+   <thead>
+     <tr>
+       <th>Pseudo</th>
+       <th>Rôle</th>
+     </tr>
+   </thead>
+   <input type="button" name="home" value="Retour" onclick="self.location.href='./home.php'">
+   <br>
+   <h2>Suppresion membre :</h2>
+   <form action="suppresion_membre.php" method="post">
+            <input type="text" name="pseudo" placeholder="Pseudo du membre"/>
+            <button type="submit" name="supp_l">Supprimer un membre</button>
+        </form>
+    <br>
+    <h2>Modification rôle membre :</h2>
+            <form action="form_modif_membre.php" method="post">
+            <input type="text" name="pseudo" placeholder="Pseudo du membre"/>
+            <button type="submit" name="modif_m">Modifier un rôle</button>
+            </form>
+    <br>
+    <br>
+   <tbody>
+     
+      <tr>
+          <?php foreach ($liste as $donnees) { ?>
+       <td><?php echo $donnees['pseudo_membre']; ?></td>
+       <td><?php echo $donnees['role']; ?></td>
+       </tr>
+    <?php
+    }
+    ?>
+    
+   </tbody>
+ </table>
+</body>
+
+</html>
