1bb9e8659df7dcbda66e072dbf6be18e7f793ce4
ilyana.megy@ynov.com <ilyana.megy@ynov.com>
2020-06-16 13:57:50 +0200

    13h57 16/06 Ilyana

--- php/inscription.php
+++ php/inscription.php
@@ -10,39 +10,38 @@
    </head>
 
    <body class="container-fluid windows">
-      <div class="container content" style="margin-top:3%;">
-
+      <div class="container content" style="margin-top:2px;">
          <a href="index.html"><img class='logo' src="../logo/logo_lighter.png" alt="BookClub logo" /></a>
                
          <div class="title_page">Inscrivez-vous</div>
          
          <form style="margin-left:4%;" method="POST" action="creation_membre.php">
                   
-            <label for="pseudo" style="padding-left:6%;" class="input_form">
+            <label for="pseudo" style="" class="input_form">
                <div>Nom utilisateur</div>
                <input name="pseudo" type="text" required/>
             </label>
 
-            <label for="mail" style="padding-left:6%;" class="input_form">
+            <label for="mail" style="" class="input_form">
                <div>Adresse Email</div>
                <input name="mail" type="text" required/>
             </label>
 
-            <label for="mdp" style="padding-left:6%;" class="input_form">
+            <label for="mdp" style="" class="input_form">
                <div>Mot de passe</div>
                <input name="mdp" type="text" required/>
             </label>
 
-            <label for="pays" style="padding-left:6%;" class="input_form">
+            <label for="pays" style="" class="input_form">
                <div>Pays</div>
                <input name="pays" type="text" required/>
             </label>
 
-            <label for="sexe" style="padding-left:6%;" class="input_form">
+            <label for="sexe" style="" class="input_form">
                <input name="sex" type="radio" value="H"> Homme <input type="radio" name="sex" value="F"> Femme
             </label>
 
-            <label for="age" style="margin-left:-1%;" class="input_form">
+            <label for="age" style="margin-left:5px;" class="input_form">
                <div>Votre âge</div>
                <input class="age" name="age" type="text" required/>
             </label>
